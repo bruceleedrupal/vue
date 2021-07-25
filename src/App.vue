bf1f54a5c3d9b568ef91ab3119e00c651dc013c2
bruceLee <brucelee.drupal@gmail.com>
2021-07-25 19:00:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  {{title}}
+  <input ref="name"/>
+  <button @click="clickhandle">clickme</button>
+  <Modal/>
+</template>
+
+<script>
+ 
+ import Modal from "./components/Modal.vue";
+
+
+export default {
+  name: 'App',
+  components:{Modal},
+  data(){
+    return {
+      title:'My first app'
+    }
+  },
+  methods:{
+    clickhandle(){
+      console.log(this.$refs.name);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
